a4c83b1255f05588c48aa2ebb8383b0825cd23ad
Athul <athulbe143gmail.com>
2018-05-18 18:26:51 +0530

    Client API jar

--- src/main/java/com/oceanprotocolclient/assets/Assetcontroller.java
+++ src/main/java/com/oceanprotocolclient/assets/Assetcontroller.java
@@ -1,14 +1,60 @@
 package com.oceanprotocolclient.assets;
 
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.oceanprotocolclient.interfaces.AssetsInterface;
 
-public class Assetcontroller implements AssetsInterface{
+public class Assetcontroller implements AssetsInterface {
+
+	@Value("${targethost}")
+	private String targetHost = "http://20.188.98.205:8000";
+
+	@Value("${assetUrl}")
+	private String assetUrl = "/api/v1/keeper/assets/metadata/";
 
 	@Override
-	public JSONObject assetsRegistration(String assetId) {
-		return null;
+	public JSONObject assetsRegistration(String publisherId, String name) {
+		JSONObject resultObject = new JSONObject();
+		String targetUrl = targetHost + assetUrl;
+		String postResp = null;
+		PostMethod post = new PostMethod(targetUrl);
+		post.setParameter("publisherId", publisherId);
+		post.setParameter("name", name);
+		
+		HttpClient httpclient = new HttpClient();
+		try {
+			httpclient.executeMethod(post);// post data to a url
+			postResp = post.getResponseBodyAsString(); // got response here
+			if (postResp == null) {
+				resultObject.put("result", "Post Response is not Present");
+				return resultObject;
+			}
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		// Convert the string into jsonobject
+		String prepostToJson = postResp.substring(1, postResp.length() - 1);
+		String postToJson = prepostToJson.replaceAll("\\\\", ""); 
+		JSONParser parser = new JSONParser();
+		JSONObject json = null;
+		try {
+			json = (JSONObject) parser.parse(postToJson);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return json;
 	}
 
 	@Override
--- src/main/java/com/oceanprotocolclient/interfaces/AssetsInterface.java
+++ src/main/java/com/oceanprotocolclient/interfaces/AssetsInterface.java
@@ -4,7 +4,7 @@ import org.json.simple.JSONObject;
 
 public interface AssetsInterface {
 
-	JSONObject assetsRegistration(String assetId);
+	JSONObject assetsRegistration(String publisherId,String name);
 	JSONObject getAssests(String assetId);
 	JSONObject updateAssets();
 	
