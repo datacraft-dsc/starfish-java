45a642753320238950db7e4ed0ed8c8ed55af02e
Aleena <aleenajohn1991@gmail.com>
2018-05-24 18:18:50 +0530

    Initial Commit

--- src/main/java/com/oceanprotocolclient/assets/Assetcontroller.java
+++ src/main/java/com/oceanprotocolclient/assets/Assetcontroller.java
@@ -7,7 +7,7 @@
  * url to register an asset   :  http://host:8000/api/v1/keeper/assets/metadata/
  * url to download/upload an asset   :  http://host:8000/api/v1/provider/assets/asset/
  * paramter :assetId
- * Author : Athul (Uvionics Tec)
+ * Author : Aleena,Athul,Arun (Uvionics Tec)
  */
 
 package com.oceanprotocolclient.assets;
@@ -41,73 +41,77 @@ import com.oceanprotocolclient.model.Asset;
 
 @SuppressWarnings("deprecation")
 public class Assetcontroller implements AssetsInterface {
-
 	/**
+	 * This method used to register an asset Json-encoded payload containing the
+	 * Asset schema with the assetId, creationDatetime and contentState filled
+	 * in.
 	 * 
-	 * @param publisherId,name
-	 * @return JSONObject
-	 * 
-	 *         Posting a url and return result into Asset Controller
+	 * Minimum required: name, publisherId * @return java object asset
 	 */
 
 	@Override
-	public Asset assetsRegistration(String publisherId, String name,String targetUrl) {
-		Asset assets = new Asset();
-		String postResp = null;
+	public Asset assetsRegistration(String publisherId, String name, String targetUrl) {
+		Asset assets = new Asset(); // Asset object creation
+		String postResp = null; // Initialize the varibale
 		// set parameters to PostMethod
 		PostMethod post = new PostMethod(targetUrl);
+		// set the parametre publisherId
 		post.setParameter("publisherId", publisherId);
+		// set the parametre name
 		post.setParameter("name", name);
-
 		HttpClient httpclient = new HttpClient();
 		try {
 			httpclient.executeMethod(post);// post data to a url
 			postResp = post.getResponseBodyAsString(); // got response here
-
-			// Used for return a Json Object with failed result and status
-
+			// check wether the ocean network response is empty or not
 			if (postResp == null) {
 				return assets;
 			}
 		} catch (IOException e) {
-
 			e.printStackTrace();
 		}
 		// Convert the string into jsonobject
 		String prepostToJson = postResp.substring(1, postResp.length() - 1);
+		// Remove "\\" from the json string from ocean network
 		String postToJson = prepostToJson.replaceAll("\\\\", "");
+		// create a json parser
 		JSONParser parser = new JSONParser();
+		// Initialise the json variable
 		JSONObject json = null;
 		try {
+			// parse the json string into json object
 			json = (JSONObject) parser.parse(postToJson);
+			// Set asset id from json object into asset
 			assets.setAssetId((String) json.get("assetId"));
+			// Set marketplaceId from json object into asset
 			assets.setMarketplaceId((String) json.get("marketplaceId"));
+			// Set publisherId from json object into asset
 			assets.setPublisherId((String) json.get("publisherId"));
-			assets.setUpdateDatetime( json.get("updateDatetime").toString());
+			// Set updateDatetime from json object into asset
+			assets.setUpdateDatetime(json.get("updateDatetime").toString());
+			// Set contentState from json object into asset
 			assets.setContentState((String) json.get("contentState"));
+			// Set name from json object into asset
 			assets.setAssetname((String) json.get("name"));
+			// Set creationDatetime from json object into asset
 			assets.setCreationDatetime(json.get("creationDatetime").toString());
-			
+
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-
-		// Used for adding response and status to Json Object
-		
 		return assets;
 	}
 
 	/**
+	 * It is used to get the asset response from ocean network Paramete
+	 * targetUrl
 	 * 
-	 * @param assetId
-	 * @return JSONObject
-	 * 
-	 *         Posting a url and return result into Asset Controller
+	 * @return java object asset GET "/api/v1/keeper/assets/metadata/{asset_id}"
 	 */
 	@Override
 	public Asset getAnAssests(String targetUrl) {
-		Asset assets = new Asset();
-		JSONObject json = null;
+		Asset assets = new Asset(); // asset object creation
+		JSONObject json = null; // initialize the json object into null
 		try {
 
 			GetMethod get = new GetMethod(targetUrl);
@@ -115,7 +119,7 @@ public class Assetcontroller implements AssetsInterface {
 			httpclient.executeMethod(get);
 			// used to get response from ocean server
 			String getResp = get.getResponseBodyAsString();
-			// Used for return a Json Object with failed result and status
+			// check the response from ocean network
 			if (getResp == null) {
 				return assets;
 			}
@@ -126,28 +130,28 @@ public class Assetcontroller implements AssetsInterface {
 			JSONParser parser = new JSONParser();
 			// parse string to json object
 			json = (JSONObject) parser.parse(postToJson);
+			// Set asset id into asset
 			assets.setAssetId((String) json.get("assetId"));
+			// Set publisherId into asset
 			assets.setPublisherId((String) json.get("publisherId"));
+			// Set name into asset
 			assets.setAssetname((String) json.get("name"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		// Used for adding response and status to Json Object
 		return assets;
 
 	}
 
 	/**
-	 * Update the asset by using the given details
+	 * Update the asset by using the given details PUT
+	 * "/api/v1/keeper/assets/metadata/{asset_id}" parametes targetUrl,asset
 	 * 
-	 * @param assetId
-	 * @param asset
 	 * @return
 	 *
 	 */
 	@Override
 	public ResponseEntity<Object> updateAssets(String targetUrl, Asset asset) {
-
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			// setting the headers for the url
@@ -155,39 +159,36 @@ public class Assetcontroller implements AssetsInterface {
 			// content-type setting
 			headers.setContentType(MediaType.APPLICATION_JSON);
 			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-			
 			// create a json object to accept the asset name
 			JSONObject assetName = new JSONObject();
-			//insert asset name to the json object
+			// insert asset name to the json object
 			assetName.put("name", asset.getAssetname());
-
 			// create and http entity to attach with the rest url
-			org.springframework.http.HttpEntity<JSONObject> entity = new org.springframework.http.HttpEntity<>(assetName, headers);
+			org.springframework.http.HttpEntity<JSONObject> entity = new org.springframework.http.HttpEntity<>(
+					assetName, headers);
+			// sent data request fro update data to ocean network
 			ResponseEntity<Object> response = restTemplate.exchange(targetUrl, HttpMethod.PUT, entity, Object.class);
-				
-				return response;
-
-			
-
+			return response;
 		} catch (Exception ex) {
 			ex.printStackTrace();
-
 		}
-
 		return null;
 	}
 
 	/**
 	 * 
-	 * @param assetId,file
+	 * parametes targetUrl,file
+	 * 
 	 * @return JSONObject
 	 * 
-	 *         Posting a url and return result into Asset Controller
+	 * Allow uploading a file for an already registered asset. The
+	 * upload is submitted to the provider. POST
+	 * "/api/v1/provider/assets/asset/"
 	 */
 
 	@SuppressWarnings({ "resource", "unchecked" })
 	@Override
-	public JSONObject uploadAssest(File file,String targetUrl) {
+	public JSONObject uploadAssest(File file, String targetUrl) {
 		JSONObject resultObject = new JSONObject();
 		// set parameters to PostMethod
 		org.apache.http.client.HttpClient client = new DefaultHttpClient();
@@ -220,6 +221,9 @@ public class Assetcontroller implements AssetsInterface {
 			return resultObject;
 		}
 	}
+	/**
+	 * Allow downloading the asset file from the provider. GET "/api/v1/provider/assets/asset/"
+	 */
 
 	@SuppressWarnings("unchecked")
 	@Override
--- src/main/java/com/oceanprotocolclient/user/UserController.java
+++ src/main/java/com/oceanprotocolclient/user/UserController.java
@@ -6,7 +6,7 @@
  * For registering an user.
  * url to register an user   :  http://host:8000/api/v1/keeper/users/user/
  * paramter :actorId
- * Author : Athul (Uvionics Tec)
+ * Author : Aleena , Athul ,Arun (Uvionics Tec)
  */
 
 package com.oceanprotocolclient.user;
@@ -32,35 +32,38 @@ import com.oceanprotocolclient.model.User;
 
 public class UserController implements UserInterface {
 
-
 	/**
 	 * 
 	 * @param actorId
 	 * @return JSONObject
 	 * 
-	 *         Posting a url and return result into user Registration
+	 * Registers an actor with the Ocean network. POST
+	 * "/api/v1/keeper/actors/actor/"
 	 */
 
-	public User userRegistration(String actorId,String targetUrl) {
+	public User userRegistration(String actorId, String targetUrl) {
+		// Create object for user class..it include all user details
 		User user = new User();
+		// Initialize postResp - response from ocean network is given to this
+		// variable
 		String postResp = null;
 		try {
-			/**
-			 * Used for generating a random Alphabet and add to ActorId
-			 */
+			// Used for generating a random Alphabet and add to ActorId
 			Random rnd = new Random();
 			char c = (char) (rnd.nextInt(26) + 'a');
 			String s = Character.toString(c);
 			actorId = s + actorId;
 
 			/**
-			 * Used for posting the targetUrl
+			 * Used for posting the data to ocean network
 			 */
 			PostMethod post = new PostMethod(targetUrl);
 			post.setParameter("actorId", actorId);// set Parameter actorId
 			HttpClient httpclient = new HttpClient();
 			httpclient.executeMethod(post);
+			// Response from ocean network
 			postResp = post.getResponseBodyAsString();
+
 			/**
 			 * Used for return a Json Object with failed result and status
 			 */
@@ -71,17 +74,27 @@ public class UserController implements UserInterface {
 			 * Used for getting WalletId and PrivateKey
 			 */
 			String prepostToJson = postResp.substring(1, postResp.length() - 1);
+			// Data coming from ocean network is a json string..This line remove
+			// the "\\" from the response
 			String postToJson = prepostToJson.replaceAll("\\\\", "");
-			JSONParser parser = new JSONParser();
+			JSONParser parser = new JSONParser(); // create json parser
+			// parse the data to json object
 			JSONObject json = (JSONObject) parser.parse(postToJson);
-			System.out.println(json);
+			// get the wallet address from ocean network response
 			String walletId = (String) json.get("defaultWalletAddress");
+			// set the wallet id to the user object
 			user.setWalletId(walletId);
+			// get the private key from ocean network response
 			String privateKey = (String) json.get("privateKey");
+			// set the private key to the user object
 			user.setPrivateKey(privateKey);
+			// set the actorid to the user object
 			user.setActorId(actorId);
-			user.setUpdateDatetime( json.get("updateDatetime").toString());
+			// set the updateDatetime to the user object
+			user.setUpdateDatetime(json.get("updateDatetime").toString());
+			// set the user state to the user object
 			user.setState((String) json.get("state"));
+			// set the creationDatetime to the user object
 			user.setCreationDatetime(json.get("creationDatetime").toString());
 			/**
 			 * Used for adding WalletId, PrivateKey,actorId and status to Json
@@ -98,21 +111,25 @@ public class UserController implements UserInterface {
 	 * @param actorId
 	 * @return JSONObject
 	 * 
-	 *         Posting a url and return result into user Registration
+	 * This method used to get the actor details from ocean network GET
+	 * "/api/v1/keeper/actors/actor/<actor_id>"
 	 */
 
-	public User getActor(String actorId,String targetUrl) {
+	public User getActor(String actorId, String targetUrl) {
+		// Create object for user class..it include all user details
 		User user = new User();
+		// Initialize postResp - response from ocean network is given to this
+		// variable
 		String getResp = null;
-		
 		/**
-		 * Used for posting the targetUrl
+		 * Used for getting the data to ocean network
 		 */
 		try {
 			String targetURL = targetUrl + actorId;
 			GetMethod get = new GetMethod(targetURL);
 			HttpClient httpclient = new HttpClient();
 			httpclient.executeMethod(get);
+			// Response from ocean network
 			getResp = get.getResponseBodyAsString();
 			/**
 			 * Used for return a Json Object with failed result and status
@@ -124,34 +141,40 @@ public class UserController implements UserInterface {
 			 * Used for getting WalletId and PrivateKey
 			 */
 			String prepostToJson = getResp.substring(1, getResp.length() - 1);
+			// Data coming from ocean network is a json string..This line remove
+			// the "\\" from the response
 			String postToJson = prepostToJson.replaceAll("\\\\", "");
-			JSONParser parser = new JSONParser();
+			JSONParser parser = new JSONParser();// create json parser
+			// parse the data to json object
 			JSONObject json = (JSONObject) parser.parse(postToJson);
-			System.out.println(json);
-			user.setWalletId( json.get("defaultWalletAddress").toString());
-			user.setActorId( json.get("actorId").toString());
-			user.setUpdateDatetime( json.get("updateDatetime").toString());
+			// set the wallet id to the user object
+			user.setWalletId(json.get("defaultWalletAddress").toString());
+			// set the actor id to the user object
+			user.setActorId(json.get("actorId").toString());
+			// set the updateDatetime to the user object
+			user.setUpdateDatetime(json.get("updateDatetime").toString());
+			// set the state to the user object
 			user.setState((String) json.get("state"));
+			// set the creationDatetime to the user object
 			user.setCreationDatetime(json.get("creationDatetime").toString());
 		} catch (Exception e) {
-		}
-		/**
-		 * Used for adding response and status
-		 */
+			e.printStackTrace();
+		  }
 		return user;
 	}
 
 	/**
-	 * To update a user based on the given the details
+	 * PUT "/api/v1/keeper/actors/actor/<actor_id>" JSON-encoded key-value pairs
+	 * from the Actor schema that are allowed to be updated (only 'name' and
+	 * 'attributes')
 	 * 
-	 * @param actorId
-	 * @param user
+	 * @param targetUrl
+	 * @param name
 	 * @return
 	 *
 	 */
 	@Override
 	public ResponseEntity<Object> updateActor(String targetUrl, String name) {
-
 		try {
 			RestTemplate restTemplate = new RestTemplate();
 			// setting the headers for the url
@@ -159,25 +182,18 @@ public class UserController implements UserInterface {
 			// content-type setting
 			headers.setContentType(MediaType.APPLICATION_JSON);
 			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-
 			// create a json object to accept the user name
 			JSONObject userName = new JSONObject();
-			//insert user name to the json object
+			// insert user name to the json object
 			userName.put("name", name);
-
 			// create and http entity to attach with the rest url
-			HttpEntity<JSONObject> entity = new HttpEntity<>(userName, headers);;
+			HttpEntity<JSONObject> entity = new HttpEntity<>(userName, headers);
+			// sent data to ocean network for updating the data
 			ResponseEntity<Object> response = restTemplate.exchange(targetUrl, HttpMethod.PUT, entity, Object.class);
-			
 			return response;
-
-		
-
-	} catch (Exception ex) {
-		ex.printStackTrace();
-
-	}
-
-	return null;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
 	}
 }
