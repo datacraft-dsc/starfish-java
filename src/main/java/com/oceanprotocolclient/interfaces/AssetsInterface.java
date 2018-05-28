e674d0ae9411a3de05d2c9010496628a8e565fcb
Aleena <aleenajohn1991@gmail.com>
2018-05-28 14:55:18 +0530

    Add some comments in the interface

--- src/main/java/com/oceanprotocolclient/interfaces/AssetsInterface.java
+++ src/main/java/com/oceanprotocolclient/interfaces/AssetsInterface.java
@@ -1,3 +1,8 @@
+/**
+ * interface used to register asset,get an asset,
+ * upload asset and download assets.
+ */
+
 package com.oceanprotocolclient.interfaces;
 
 import java.io.File;
@@ -8,9 +13,43 @@ import org.springframework.http.ResponseEntity;
 import com.oceanprotocolclient.model.Asset;
 
 public interface AssetsInterface {
+	/**
+	 * Register an asset to ocean network.
+	 * 
+	 * @param publisherId - Publisher Id to register asset
+	 * @param name - Publisher name to register asset
+	 * @param targetUrl - Ocean network host and port 
+	 * @return
+	 */
 	Asset assetsRegistration(String publisherId,String name,String targetUrl);
+	/**
+	 * To get asset details from ocean network
+	 * 
+	 * @param targetUrl - Ocean network host and port 
+	 * @return
+	 */
 	Asset getAnAssests(String targetUrl);
+	/**
+	 * Update asset meta data
+	 * 
+	 * @param targetUrl - Ocean network host and port
+	 * @param asset - Asset object with updated details
+	 * @return
+	 */
 	ResponseEntity<Object> updateAssets(String targetUrl,Asset asset);
+	/**
+	 * Allow uploading a file for an already registered asset. 
+	 * The upload is submitted to the provider.
+	 * @param file  - File to be uploaded
+	 * @param targetUrl - Ocean network host and port
+	 * @return
+	 */
 	JSONObject uploadAssest(File file,String targetUrl);
+	/**
+	 * Allow downloading the asset file from the provider
+	 * 
+	 * @param targetUrl - Ocean network host and port
+	 * @return
+	 */
 	JSONObject downloadAssest(String targetUrl);
 }
--- src/main/java/com/oceanprotocolclient/interfaces/UserInterface.java
+++ src/main/java/com/oceanprotocolclient/interfaces/UserInterface.java
@@ -5,10 +5,36 @@ import org.springframework.http.ResponseEntity;
 import com.oceanprotocolclient.model.User;
 
 public interface UserInterface {
-
+	/**
+	 * Used to registers an actor with the Ocean network
+	 * 
+	 * @param actorId - Id of an actor
+	 * @param targetUrl -  Ocean network host and port 
+	 * @return
+	 */
 	User userRegistration(String actorId,String targetUrl);
+	/**
+	 * Get actor information
+	 * 
+	 * @param actorId - Id of an actor
+	 * @param targetUrl - Ocean network host and port 
+	 * @return
+	 */
 	User getActor(String actorId,String targetUrl);
+	/**
+	 * Update actor name
+	 * 
+	 * @param targetUrl - Ocean network host and port 
+	 * @param name - actor name
+	 * @return
+	 */
 	ResponseEntity<Object> updateActor(String targetUrl, String name);
+	/**
+	 * 
+	 * @param targetUrl - Ocean network host and port 
+	 * @param id - To disable the actor
+	 * @return
+	 */
 	ResponseEntity<Object> disableActor(String targetUrl, String id);
 	}
 
