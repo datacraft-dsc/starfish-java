434a6d97c529c7a6eba747e197713e06b0b1b82f
ARUN JOHNSON <arun.johnson@citta.ai>
2018-05-31 19:24:58 +0530

    test folder added to Project

---
+++ src/test/java/com/oceanprotocolclient/AssetControllerTest.java
@@ -0,0 +1,26 @@
+package com.oceanprotocolclient;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.oceanprotocolclient.assets.Assetcontroller;
+
+public class AssetControllerTest {
+	
+	Assetcontroller ac;
+	
+	
+	@Before
+	public void init()
+	{
+		ac = new Assetcontroller();
+	}
+	
+	@Test
+	public void TestMethod()
+	{
+		System.out.println("HHHHHHHHHHHHHHHHHHh");
+	}
+
+}
