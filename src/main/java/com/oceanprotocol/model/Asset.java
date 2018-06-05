7f87fc6e55a1c3ec44d2eb22d134e9325ca8e534
Athul <athulbe143gmail.com>
2018-06-05 17:24:20 +0530

    Model Class Updations

--- src/main/java/com/oceanprotocol/model/Asset.java
+++ src/main/java/com/oceanprotocol/model/Asset.java
@@ -9,53 +9,17 @@ import java.util.Map;
 import org.json.simple.JSONObject;
 
 public class Asset {
-	private String publisherId;
-	private String assetId;
-	private String assetName;
 	/**
 	 * {@link Map} to save the response from the ocean network as a key value
 	 * pair
 	 */
 	private Map<String, JSONObject> oceanResponse;
-	private String contractId;
 
 	/**
 	 * Construct the oceanResponse with Hash map for further use.
 	 */
 	public Asset() {
 		this.oceanResponse = new HashMap<>();
-	}
-
-	public String getPublisherId() {
-		return publisherId;
-	}
-
-	public void setPublisherId(String publisherId) {
-		this.publisherId = publisherId;
-	}
-
-	public String getAssetId() {
-		return assetId;
-	}
-
-	public void setAssetId(String assetId) {
-		this.assetId = assetId;
-	}
-
-	public String getAssetName() {
-		return assetName;
-	}
-
-	public void setAssetName(String assetName) {
-		this.assetName = assetName;
-	}
-
-	public String getContractId() {
-		return contractId;
-	}
-
-	public void setContractId(String contractId) {
-		this.contractId = contractId;
 	}
 
 	public Map<String, JSONObject> getOceanResponse() {
@@ -68,10 +32,7 @@ public class Asset {
 
 	@Override
 	public String toString() {
-		return "Asset [publisherId=" + publisherId + ", assetId=" + assetId + ", assetName=" + assetName
-				+ ", oceanResponse=" + oceanResponse + ", contractId=" + contractId + "]";
+		return "Asset [oceanResponse=" + oceanResponse + "]";
 	}
-
-	
 
 }
