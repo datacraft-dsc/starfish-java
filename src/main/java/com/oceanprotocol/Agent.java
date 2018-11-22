5ab688a632f155fd219c58806b3e6eee9c65dd2e
Mike <mike@mikera.net>
2018-11-22 09:52:30 +0800

    Add DDO lookup for Agent

--- src/main/java/com/oceanprotocol/Agent.java
+++ src/main/java/com/oceanprotocol/Agent.java
@@ -1,5 +1,7 @@
 package com.oceanprotocol;
 
+import org.json.simple.JSONObject;
+
 /**
  * Class representing an Agent in the Ocean Ecosystem
  * 
@@ -10,4 +12,25 @@ package com.oceanprotocol;
  */
 public class Agent {
 
+	private JSONObject ddo;
+
+	/**
+	 * Gets the cached DDO for the agent
+	 * Fetches the latest DDO from Universal Resolver if not cached
+	 * @return
+	 */
+	public JSONObject getDDO() {
+		if (ddo==null) {
+			ddo=refreshDDO();
+		}
+		return ddo;
+	}
+	
+	/**
+	 * Fetches the latest DDO from Universal Resolver if not cached
+	 * @return
+	 */
+	public JSONObject refreshDDO() {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 }
