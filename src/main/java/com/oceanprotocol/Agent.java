0434f027896e0c72e19a77f5d0b8761d4a4ca5d7
Mike <mike@mikera.net>
2018-11-22 08:59:28 +0800

    add Agent class

---
+++ src/main/java/com/oceanprotocol/Agent.java
@@ -0,0 +1,13 @@
+package com.oceanprotocol;
+
+/**
+ * Class representing an Agent in the Ocean Ecosystem
+ * 
+ * Agents are addressed with a W3C DID
+ * 
+ * @author Mike
+ *
+ */
+public class Agent {
+
+}
