4d478cd872a3151fa3a4bb563742ef425575ca6f
Mike <mike@mikera.net>
2019-02-14 09:34:07 +0800

    Docs

--- src/main/java/sg/dex/starfish/Asset.java
+++ src/main/java/sg/dex/starfish/Asset.java
@@ -15,5 +15,11 @@ public interface Asset {
 	 */
 	public JSONObject getMetadata();
 
+	/**
+	 * Returns true if this asset is a data asset, i.e. the asset represents an immutable
+	 * data object.
+	 * 
+	 * @return
+	 */
 	public boolean isDataAsset();
 }
--- src/main/java/sg/dex/starfish/Listing.java
+++ src/main/java/sg/dex/starfish/Listing.java
@@ -2,6 +2,9 @@ package sg.dex.starfish;
 
 /**
  * Interface representing a listing of an asset on a data marketplace
+ * 
+ * Listings can be used to query and purchase Assets
+ * 
  * @author Mike
  *
  */
