4d42ba88572f0c33970461c54a9bd38b6d95b2b3
Mike <mike@mikera.net>
2019-02-15 08:30:27 +0800

    getContentIDs for asset bundle

--- src/main/java/sg/dex/starfish/AssetBundle.java
+++ src/main/java/sg/dex/starfish/AssetBundle.java
@@ -1,5 +1,11 @@
 package sg.dex.starfish;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+
 /**
  * Class representing an asset bundle
  * @author Mike
@@ -15,5 +21,30 @@ public class AssetBundle extends AAsset {
 	public boolean isDataAsset() {
 		return false;
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<String> getContentIDs() {
+		JSONObject meta=getMetadata();
+		Object contentValue=meta.get("contents");
+		if (!(contentValue instanceof Map)) {
+			throw new Error("Expected a map of contents in bundle, found: "+contentValue);
+		}
+		Map<String,Object> contents = (Map<String,Object>)contentValue;
+		ArrayList<String> ids=new ArrayList<String>(contents.size());
+		for (Object o: contents.values()) {
+			Map<String,Object> content=(Map<String,Object>)o;
+			String id=(String)content.get("assetID");
+			ids.add(id);
+		}
+		return ids;
+	}
 
+	/**
+	 * Gets an asset contained within this bundle
+	 * @param path
+	 * @return
+	 */
+	public Asset getContent(String path) {
+		throw new UnsupportedOperationException();
+	}
 }
