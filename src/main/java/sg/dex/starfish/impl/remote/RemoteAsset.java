fa2edccf54ddbe0930ec2c7f172c2ef7eea24257
Mike <mike@mikera.net>
2019-02-19 12:10:58 +0800

    Refactor RemoteAsset to use URL as internal field

--- src/main/java/sg/dex/starfish/impl/remote/RemoteAsset.java
+++ src/main/java/sg/dex/starfish/impl/remote/RemoteAsset.java
@@ -10,16 +10,31 @@ import sg.dex.starfish.Asset;
 import sg.dex.starfish.DataAsset;
 import sg.dex.starfish.util.TODOException;
 
+/**
+ * Class representing a data asset referenced by a URL.
+ * 
+ * @author Mike
+ *
+ */
 public class RemoteAsset extends ADataAsset implements DataAsset {
 
-	private final String urlString;
+	private final URL url;
 	
-	protected RemoteAsset(String meta, String urlString) {
+	protected RemoteAsset(String meta, URL url) {
 		super(meta);
-		this.urlString=urlString;
+		this.url=url;
 	}
 	
-	public static Asset create(String meta, String url) {
+	public static Asset create(String meta, String urlString) {
+		try {
+			return new RemoteAsset(meta,new URL(urlString));
+		}
+		catch (MalformedURLException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
+	public static Asset create(String meta, URL url) {
 		return new RemoteAsset(meta,url);
 	}
 
@@ -44,17 +59,12 @@ public class RemoteAsset extends ADataAsset implements DataAsset {
 		throw new TODOException();
 	}
 	
-	private String getURLString() {
-		return urlString;
+	public String getURLString() {
+		return url.toString();
 	}
 
 	public URL getURL() {
-		try {
-			return new URL(getURLString());
-		}
-		catch (MalformedURLException e) {
-			throw new Error(e);
-		}
+		return url;
 	}
 
 
