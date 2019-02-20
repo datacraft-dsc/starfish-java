dc2b924f2710a3873f2ffe75174441c77179a381
Mike <mike@mikera.net>
2019-02-20 08:25:33 +0800

    Stub out MemoryJob using Future

--- src/main/java/sg/dex/starfish/Job.java
+++ src/main/java/sg/dex/starfish/Job.java
@@ -19,4 +19,18 @@ public interface Job {
 	 * @return
 	 */
 	public Asset getResult();
+	
+	
+	/**
+	 * Waits for the result of the Operation and returns the result Asset
+	 * WARNING: may never return if the job does not complete 
+	 */
+	public Asset awaitResult();
+	
+	/**
+	 * Waits for the result of the Operation and returns the result Asset
+	 * or returns null if the timeout in milliseconds expires before the
+	 * asset is available.
+	 */
+	public Asset awaitResult(long timeoutMillis);
 }
--- src/main/java/sg/dex/starfish/impl/memory/MemoryJob.java
+++ src/main/java/sg/dex/starfish/impl/memory/MemoryJob.java
@@ -1,20 +1,72 @@
 package sg.dex.starfish.impl.memory;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import sg.dex.starfish.Asset;
 import sg.dex.starfish.Job;
 
+/**
+ * Class representing a job being conducted in the local JVM
+ * 
+ * @author Mike
+ *
+ */
 public class MemoryJob implements Job {
 
-	private Asset result=null;
+	private final Future<Asset> future;
 	
+	public MemoryJob(Future<Asset> future) {
+		this.future=future;
+	}
+
 	@Override
 	public boolean isComplete() {
-		return result!=null;
+		return future.isDone();
+	}
+	
+	public MemoryJob create(Future<Asset> future) {
+		return new MemoryJob(future);
 	}
 
 	@Override
 	public Asset getResult() {
-		return result;
+		try {
+			return future.isDone()?future.get():null;
+		}
+		catch (InterruptedException e) {
+			throw new Error("Job interruped",e);
+		}
+		catch (ExecutionException e) {
+			throw new Error("Job failed with exception: ",e.getCause());
+		}
+	}
+
+	@Override
+	public Asset awaitResult() {
+		try {
+			return future.get();
+		}
+		catch (InterruptedException e) {
+			throw new Error("Job interruped",e);
+		}
+		catch (ExecutionException e) {
+			Throwable cause=e.getCause();
+			throw new Error("Job failed with exception: "+cause,e);
+		}
+	}
+
+	@Override
+	public Asset awaitResult(long timeoutMillis) {
+		try {
+			return future.get(timeoutMillis, TimeUnit.MILLISECONDS);
+		}
+		catch (InterruptedException | ExecutionException | TimeoutException e) {
+			Throwable cause=e.getCause();
+			throw new Error("Job failed with exception: "+cause,e);
+		}
 	}
 
 }
