57998ce113fdfb92dfe5a1c65333f2c9cd8ee409
Tom Marble <tmarble@info9.net>
2019-02-21 17:19:12 -0600

    Turn on rsync debugging for docs deploy

--- scripts/deployDocs.sh
+++ scripts/deployDocs.sh
@@ -53,7 +53,7 @@ if [ ! -z "$DEPLOY_SERVER" ]; then
         mkdir -p "target/shrimp/$PROJECT_NAME/branches/$DEV_BRANCH"
         mv "$DEPLOY_FILENAME" "target/shrimp/$PROJECT_NAME/branches/$DEV_BRANCH/"
         env > "target/shrimp/$PROJECT_NAME/branches/$DEV_BRANCH/env.txt"
-        rsync -auW --rsh 'ssh -i /tmp/dex-docs-deploy' target/shrimp/ ${DEPLOY_USER}@${DEPLOY_SERVER}:./
+        rsync -auvW --rsh 'ssh -i /tmp/dex-docs-deploy' target/shrimp/ ${DEPLOY_USER}@${DEPLOY_SERVER}:
     fi
 
     rm /tmp/dex-docs-deploy
