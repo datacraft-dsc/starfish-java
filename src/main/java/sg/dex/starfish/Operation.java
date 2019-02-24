01528aeff128261c416f6a8af8a9fbf4905bd8ce
Mike <mike@mikera.net>
2019-02-24 17:59:47 +0800

    Doc edits

--- src/main/java/sg/dex/starfish/Operation.java
+++ src/main/java/sg/dex/starfish/Operation.java
@@ -24,7 +24,9 @@ public interface Operation extends Asset {
 	public Job invoke(Asset... params);
 	
 	/**
-	 * Invokes this operation with the given named parameters.
+	 * Invokes this operation with the given named parameters. Operations should override
+	 * this method to provide an implementation of asynchronous invocation via the
+	 * Job interface
 	 * 
 	 * @param params Positional parameters for this invoke job
 	 * @throws IllegalArgumentException if required parameters are not available.
@@ -33,7 +35,9 @@ public interface Operation extends Asset {
 	public Job invoke(Map<String,Asset> params);
 
 	/**
-	 * Returns the parameter specification for this operation.
+	 * Returns the parameter specification for this operation. Operations must
+	 * override this method to define what parameters they accept.
+	 * 
 	 * TODO: add brief description of format and link to DEP6
 	 * 
 	 * @return A map of parameter names to specifications
