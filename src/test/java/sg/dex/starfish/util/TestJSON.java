c1d45e9907f8d334e67102703e28cb04f23ffae2
Mike <mike@mikera.net>
2019-02-24 18:10:20 +0800

    More JSON tests

--- src/test/java/sg/dex/starfish/util/TestJSON.java
+++ src/test/java/sg/dex/starfish/util/TestJSON.java
@@ -1,6 +1,7 @@
 package sg.dex.starfish.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.util.Map;
 
@@ -21,5 +22,19 @@ public class TestJSON {
 	@Test public void testLong() {
 		Object json=JSON.parse("1");
 		assertEquals(Long.valueOf(1),json);
+	}
+	
+	@Test public void testBoolean() {
+		assertEquals(Boolean.valueOf(true),JSON.parse("true"));
+		assertEquals(Boolean.valueOf(false),JSON.parse("false"));
+	}
+	
+	@Test public void testNull() {
+		assertNull(JSON.parse("null"));
+	}
+	
+	@Test public void testRoundTrip() {
+		Map<String,Object> json=JSON.toMap("{\"foo\": 1}");
+		assertEquals(Long.valueOf(1),json.get("foo"));
 	}
 }
