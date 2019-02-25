760908a5949648aae724812a7dfe816327242c6b
Mike <mike@mikera.net>
2019-02-25 10:28:27 +0800

    Parameterised tests for JSON

---
+++ src/test/java/sg/dex/starfish/util/ParamTestJSON.java
@@ -0,0 +1,41 @@
+package sg.dex.starfish.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ParamTestJSON {
+	private String data;
+
+	public ParamTestJSON(String label,String data) {
+		this.data = data;
+	}
+
+	@Parameterized.Parameters(name = "{index}: {0}")
+	public static Collection<String[]> dataExamples() {
+		return Arrays.asList(new String[][] { 
+			{ "Number","1" }, 
+			{ "Number with decimal","1.0" }, 
+			{ "Null","null" }, 
+			{ "String","\"foobar\"" }, 
+			{ "Array of numbers","[1,2,3]" }, 
+			{ "Array of stuff","[1,{},true,\"bar\"]" }, 
+			{ "Empty map","{}" }, 
+			{ "Nested maps", "{\"foo\": {}}" } 
+		});
+	}
+	
+	@Test public void testJSONRoundTripPretty() {
+		Object o1=JSON.parse(data);
+		String ps1=JSON.toPrettyString(o1);
+		Object o2=JSON.parse(ps1);
+		String ps2=JSON.toPrettyString(o2);
+		assertEquals(ps1,ps2);
+	}
+}
