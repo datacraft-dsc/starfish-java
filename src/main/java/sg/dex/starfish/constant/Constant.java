6d0ee04dec2f91604466134e135fd26a30351d1b
ayush.kumar@dex.sg <ayush@Ayushs-MacBook-Pro.local>
2019-03-15 11:58:08 +0800

    added testcase for memory Asser and few code refactor

---
+++ src/main/java/sg/dex/starfish/constant/Constant.java
@@ -0,0 +1,14 @@
+package sg.dex.starfish.constant;
+
+
+public class Constant {
+
+    public static final String CONTENT_HASH ="contentHash";
+    public static final String TYPE ="type";
+    public static final String DATE_CREATED ="dateCreated";
+    public static final String SIZE ="size";
+    public static final String CONTENT_TYPE ="contentType";
+    public static final String ID="id";
+
+
+}
--- src/main/java/sg/dex/starfish/impl/memory/MemoryAsset.java
+++ src/main/java/sg/dex/starfish/impl/memory/MemoryAsset.java
@@ -6,6 +6,11 @@
   */
 package sg.dex.starfish.impl.memory;
 
+import sg.dex.crypto.Hash;
+import sg.dex.starfish.Asset;
+import sg.dex.starfish.impl.ADataAsset;
+import sg.dex.starfish.util.*;
+
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -13,15 +18,7 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-import sg.dex.crypto.Hash;
-import sg.dex.starfish.Asset;
-import sg.dex.starfish.DataAsset;
-import sg.dex.starfish.impl.ADataAsset;
-import sg.dex.starfish.util.Hex;
-import sg.dex.starfish.util.AuthorizationException;
-import sg.dex.starfish.util.StorageException;
-import sg.dex.starfish.util.JSON;
-import sg.dex.starfish.util.Utils;
+import static sg.dex.starfish.constant.Constant.*;
 
 /**
  * Class representing a local in-memory asset.
@@ -49,7 +46,7 @@ public class MemoryAsset extends ADataAsset {
 		if (asset instanceof MemoryAsset) {
 			return (MemoryAsset)asset;
 		} else if (asset.isDataAsset()) {
-			byte[] data=((DataAsset)asset).getContent();
+			byte[] data=asset.getContent();
 			return new MemoryAsset(asset.getMetadataString(),data);
 		} else {
 			throw new IllegalArgumentException("Asset must be a data asset");
@@ -79,7 +76,7 @@ public class MemoryAsset extends ADataAsset {
 	 */
 	public static Asset create(String string) {
 		byte[] bytes=string.getBytes(StandardCharsets.UTF_8);
-		return create(Utils.mapOf("contentType","text/plain; charset=utf-8"),bytes);
+		return create(Utils.mapOf(CONTENT_TYPE,"text/plain; charset=utf-8"),bytes);
 	}
 
 	/**
@@ -105,20 +102,17 @@ public class MemoryAsset extends ADataAsset {
 	private static String buildMetaData(byte[] data,Map<String,Object> meta) {
 		String hash=Hex.toString(Hash.keccak256(data));
 
-		Map<String,Object> ob=new HashMap<>();
-		ob.put("dateCreated", Instant.now().toString());
-		ob.put("contentHash", hash);
-		ob.put("type", "dataset");
-		ob.put("size", Integer.toString(data.length));
-		ob.put("contentType","application/octet-stream");
-
-		if (meta!=null) {
-			for (Map.Entry<String,Object> me:meta.entrySet()) {
-				ob.put(me.getKey(), me.getValue());
-			}
+		if(meta == null){
+			meta =  new HashMap<>();
 		}
+		meta.put(DATE_CREATED, Instant.now().toString());
+		meta.put(CONTENT_HASH, hash);
+		meta.put(TYPE, "dataset");
+		meta.put(SIZE, Integer.toString(data.length));
+		meta.put(CONTENT_TYPE,"application/octet-stream");
 
-		return JSON.toString(ob);
+
+		return JSON.toString(meta);
 	}
 
 	@Override
@@ -161,7 +155,7 @@ public class MemoryAsset extends ADataAsset {
 	public Map<String,Object> getParamValue() {
 		Map<String,Object> o=new HashMap<>();
 		// pass the asset ID, i.e. hash of content
-		o.put("id", getAssetID());
+		o.put(ID, getAssetID());
 		return o;
 	}
 
