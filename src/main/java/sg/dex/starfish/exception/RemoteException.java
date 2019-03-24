db9ec07df43bccfeac32b006577aaaf558a5fb73
Mike <mike@mikera.net>
2019-03-24 15:41:46 +0800

    Docs and annotations

--- src/main/java/sg/dex/starfish/exception/RemoteException.java
+++ src/main/java/sg/dex/starfish/exception/RemoteException.java
@@ -1,10 +1,11 @@
 package sg.dex.starfish.exception;
 
 /**
- * Class representing a failure in a remote operation
+ * Class representing an unexpected failure in a remote operation
  * @author Mike
  *
  */
+@SuppressWarnings("serial")
 public class RemoteException extends RuntimeException {
 
 	public RemoteException(String message) {
