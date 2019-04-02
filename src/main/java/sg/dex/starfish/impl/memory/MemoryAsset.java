d3b85e880634ecbbdf4e5b720dd0e7a74516cdc1
Mike <mike@mikera.net>
2019-04-02 19:16:33 +0800

    Demo edits

--- src/main/java/sg/dex/starfish/impl/memory/MemoryAsset.java
+++ src/main/java/sg/dex/starfish/impl/memory/MemoryAsset.java
@@ -36,85 +36,87 @@ public class MemoryAsset extends ADataAsset {
 
 	private byte[] data;
 
-	private MemoryAsset(String meta, byte[] data){
+	private MemoryAsset(String meta, byte[] data) {
 		super(meta);
-		this.data=data;
+		this.data = data;
 	}
 
 	/**
 	 * Gets a MemoryAsset using the content and metadata from the provided asset
+	 * 
 	 * @param asset The asset to use to construct this MemoryAsset
 	 * @return A new MemoryAsset containing the data from the passed asset argument
 	 */
 	public static MemoryAsset create(Asset asset) {
 		if (asset instanceof MemoryAsset) {
-			return (MemoryAsset)asset;
+			return (MemoryAsset) asset;
 		} else if (asset.isDataAsset()) {
-			byte[] data=asset.getContent();
-			return new MemoryAsset(asset.getMetadataString(),data);
+			byte[] data = asset.getContent();
+			return new MemoryAsset(asset.getMetadataString(), data);
 		} else {
 			throw new IllegalArgumentException("Asset must be a data asset");
 		}
 	}
 
 	/**
-	 * Creates a MemoryAsset with the provided data. Default metadata will be generated.
+	 * Creates a MemoryAsset with the provided data. Default metadata will be
+	 * generated.
 	 *
 	 * @param data Byte array containing the data for this asset
 	 * @return The newly created in-memory asset
 	 */
 	public static MemoryAsset create(byte[] data) {
-		if(data == null){
+		if (data == null) {
 			throw new IllegalArgumentException("Missing data");
 		}
-		return create(buildMetaData(data,null),data);
+		return create(buildMetaData(data, null), data);
 	}
 
-
 	/**
-	 * Creates a MemoryAsset with the provided string data, encoded in UTF_8
-	 * Default metadata will be generated.
+	 * Creates a MemoryAsset with the provided string data, encoded in UTF_8 Default
+	 * metadata will be generated.
 	 *
 	 * @param string String containing the data for this asset
 	 * @return The newly created in-memory asset
 	 */
 	public static Asset create(String string) {
-		byte[] bytes=string.getBytes(StandardCharsets.UTF_8);
-		return create(Utils.mapOf(CONTENT_TYPE,"text/plain; charset=utf-8"),bytes);
+		byte[] bytes = string.getBytes(StandardCharsets.UTF_8);
+		return create(Utils.mapOf(CONTENT_TYPE, "text/plain; charset=utf-8"), bytes);
 	}
 
 	/**
-	 * Creates a MemoryAsset with the provided metadata an content
+	 * Creates a MemoryAsset with the provided metadata and content
+	 * 
 	 * @param meta A map containing the metadata for this asset
 	 * @param data Byte array containing the data for this asset
 	 * @return The newly created in-memory asset
 	 */
-	public static MemoryAsset create(Map<String,Object> meta, byte[] data) {
-
-		return create(buildMetaData(data,meta),data);
+	public static MemoryAsset create(Map<String, Object> meta, byte[] data) {
+		return create(buildMetaData(data, meta), data);
 	}
 
 	public static MemoryAsset create(String meta, byte[] data) {
-		return new MemoryAsset(meta,data);
+		return new MemoryAsset(meta, data);
 	}
 
 	/**
 	 * Build default metadata for an in-memory asset
+	 * 
 	 * @param data Asset data
 	 * @return The default metadata as a String
 	 */
-		private static String buildMetaData(byte[] data,Map<String,Object> meta) {
-		String hash=Hex.toString(Hash.keccak256(data));
+	private static String buildMetaData(byte[] data, Map<String, Object> meta) {
+		String hash = Hex.toString(Hash.keccak256(data));
 
-		Map<String,Object> ob=new HashMap<>();
+		Map<String, Object> ob = new HashMap<>();
 		ob.put(DATE_CREATED, Instant.now().toString());
 		ob.put(CONTENT_HASH, hash);
 		ob.put(TYPE, "dataset");
 		ob.put(SIZE, Integer.toString(data.length));
-		ob.put(CONTENT_TYPE,"application/octet-stream");
+		ob.put(CONTENT_TYPE, "application/octet-stream");
 
-		if (meta!=null) {
-			for (Map.Entry<String,Object> me:meta.entrySet()) {
+		if (meta != null) {
+			for (Map.Entry<String, Object> me : meta.entrySet()) {
 				ob.put(me.getKey(), me.getValue());
 			}
 		}
@@ -136,7 +138,7 @@ public class MemoryAsset extends ADataAsset {
 	 */
 	@Override
 	public InputStream getContentStream() {
-		if (data==null) throw new Error("MemoryAsset has not been initialised with data");
+		if (data == null) throw new Error("MemoryAsset has not been initialised with data");
 		return new ByteArrayInputStream(data);
 	}
 
@@ -159,12 +161,11 @@ public class MemoryAsset extends ADataAsset {
 	}
 
 	@Override
-	public Map<String,Object> getParamValue() {
-		Map<String,Object> o=new HashMap<>();
+	public Map<String, Object> getParamValue() {
+		Map<String, Object> o = new HashMap<>();
 		// pass the asset ID, i.e. hash of content
 		o.put(ID, getAssetID());
 		return o;
 	}
-
 
 }
