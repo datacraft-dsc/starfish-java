3dc4c5de28e490b63b135f1dfd05f75156ca1979
AyushVerma2 <ayushverma2@gmail.com>
2019-04-05 17:53:04 +0800

    update the Asset bundle code and test cases as discussed

--- src/main/java/sg/dex/starfish/impl/memory/MemoryAssetBundle.java
+++ src/main/java/sg/dex/starfish/impl/memory/MemoryAssetBundle.java
@@ -1,125 +1,92 @@
 package sg.dex.starfish.impl.memory;
 
-import sg.dex.crypto.Hash;
 import sg.dex.starfish.Asset;
 import sg.dex.starfish.impl.AAsset;
-import sg.dex.starfish.util.Hex;
 import sg.dex.starfish.util.JSON;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import static sg.dex.starfish.constant.Constant.*;
+import static sg.dex.starfish.constant.Constant.TYPE;
 
 /**
  * This class is to create a memory bundle asset
  */
 public class MemoryAssetBundle extends AAsset {
-    private final Map<String, Asset> subAssetMap = new HashMap<>();
 
-    /**
-     * Constructor to create an instance of MemoryAssetBundle
-     *
-     * @param meta     meta data of memory asset bundle
-     * @param subAsset sub asset
-     */
-    private MemoryAssetBundle(String meta, Asset subAsset) {
-        super(meta);
-        subAssetMap.put(subAsset.getAssetID(), subAsset);
-    }
+    private MemoryAssetBundle(String metaData) {
 
-    /**
-     * API to create the instance of memory bundle asset from list of subAsset
-     *
-     * @param meta
-     * @param subAssetList
-     */
-    private MemoryAssetBundle(String meta, List<Asset> subAssetList) {
-        super(meta);
-
-        for (Asset asset : subAssetList)
-            subAssetMap.put(asset.getAssetID(), asset);
-    }
-
-    /**
-     * Create a BundleAsset with given subAsset and the meta data
-     *
-     * @param subAsset sub asset for this BundleAsset
-     * @return A new BundleAsset with sub Asset passed
-     */
-    public static MemoryAssetBundle create(Map<String, Object> meta, Asset subAsset) {
-        return new MemoryAssetBundle(buildMetaData(meta, new String(subAsset.getContent())), subAsset);
+        super(metaData);
 
     }
 
     /**
      * Gets a BundleAsset using the content and metadata from the provided asset
      *
-     * @param asset The asset to use to construct this BundleAsset
-     * @return A new BundleAsset containing the data from the passed asset argument
+     * @param name
+     * @param subAssetId
+     * @return
      */
-    public static MemoryAssetBundle create(Asset asset) {
 
-        return new MemoryAssetBundle(buildMetaData(null, new String(asset.getContent())), asset);
+    public static MemoryAssetBundle create(String bundleName, String name, String subAssetId) {
+        //build meta data
+        Map<String, String> subAssetIdMap = new HashMap<>();
+        subAssetIdMap.put(name, "AssetId : " + subAssetId);
+
+        return new MemoryAssetBundle(buildMetaData(bundleName, subAssetIdMap));
 
     }
 
     /**
-     * API to create Bundle Asset from list of subAsset
+     * Gets a BundleAsset using the content and metadata from the provided asset
      *
-     * @param assetList
+     * @param name
+     * @param
      * @return
      */
-    public static MemoryAssetBundle create(List<Asset> assetList) {
-
-        return new MemoryAssetBundle(buildMetaData(null, getContent(assetList)), assetList);
+    public static MemoryAssetBundle create(String bundleName, String name, Asset subAsset) {
+        //build meta data
+        Map<String, String> subAssetIdMap = new HashMap<>();
+        subAssetIdMap.put(name, "AssetId : " + subAsset.getAssetID());
+        return new MemoryAssetBundle(buildMetaData(bundleName, subAssetIdMap));
     }
 
     /**
-     * API to create bundle asset from List of sub-Asset and the metaData
+     * Gets a BundleAreturn new MemoryAssetBundle(buildMetaData(bundleName, subAssetIdMap));   sset using the content and metadata from the provided asset
      *
-     * @param meta
-     * @param assetList
+     * @param
+     * @param
      * @return
      */
-    public static MemoryAssetBundle create(Map<String, Object> meta, List<Asset> assetList) {
+    public static MemoryAssetBundle create(String bundleName, Map<String, String> assetMap) {
 
-        return new MemoryAssetBundle(buildMetaData(meta, getContent(assetList)), assetList);
+        //build meta data
+        Map<String, String> subAssetIdMap = new HashMap<>();
+        for (String name : assetMap.keySet()) {
+
+            subAssetIdMap.put(name, "AssetId: " + assetMap.get(name));
+        }
+        return new MemoryAssetBundle(buildMetaData(bundleName, subAssetIdMap));
+
+
     }
 
-    private static String getContent(List<Asset> assetList) {
-        StringBuilder allContent = new StringBuilder();
-        for (Asset asset : assetList)
-            allContent.append(asset.getContent());
-        return allContent.toString();
-    }
 
     /**
      * Build default metadata for an in-bundle asset
      *
-     * @param meta BundleAsset data
      * @return The default metadata as a String
      */
-    private static String buildMetaData(Map<String, Object> meta, String content) {
-        // ToDO : need to discuss
-        String hash = Hex.toString(Hash.keccak256(content));
-
-        Map<String, Object> ob = new HashMap<>();
-        ob.put(DATE_CREATED, Instant.now().toString());
-        ob.put(CONTENT_HASH, hash);
-        ob.put(TYPE, "bundle");
-        ob.put(CONTENT_TYPE, "application/octet-stream");
-
-        if (meta != null) {
-            for (Map.Entry<String, Object> me : meta.entrySet()) {
-                ob.put(me.getKey(), me.getValue());
-            }
+    private static String buildMetaData(String bundleName, Map<String, String> content) {
+        if (null == content) {
+            content = new HashMap<>();
         }
 
-        return JSON.toString(ob);
+        Map<String, Object> ob = new HashMap<>();
+        ob.put("name", bundleName);
+        ob.put(TYPE, "bundle");
+        ob.put("contents", content);
+        return JSON.toPrettyString(ob);
     }
 
     @Override
@@ -137,19 +104,37 @@ public class MemoryAssetBundle extends AAsset {
      *
      * @return it will return a map of all Sub asset present in the Asset Bundle
      */
-    public Map<String, Asset> getAllSubAsset() {
-        return subAssetMap;
+    public Map<String, String> getAllSubAsset() {
+        return (Map<String, String>) getMetadata().get("contents");
 
     }
 
     /**
      * API to get the sub Asset based on sub Asset ID
      *
-     * @param id sub Asset ID
+     * @param name sub Asset ID
      * @return
      */
-    public Asset getSubAssetById(String id) {
-        return subAssetMap.get(id);
+    public String getSubAssetById(String name) {
+        Map<String, String> metaData = (Map<String, String>) getMetadata().get("contents");
+        return metaData.get(name);
+    }
+
+
+    /**
+     * API to add subAsset .
+     * It will create a new instance of Assetbundle instance and all the give sub-Asset
+     *
+     * @param
+     * @return
+     */
+    public MemoryAssetBundle addSubAsset(String name, String subAssetId) {
+        Map<String, Object> metaData = getMetadata();
+        Map<String, String> content = (Map<String, String>) metaData.get("contents");
+        String bundleName = metaData.get("name").toString();
+        content.put(name, "AssetId: "+ subAssetId);
+        return create(bundleName, content);
+
     }
 
     /**
@@ -159,25 +144,12 @@ public class MemoryAssetBundle extends AAsset {
      * @param subAsset
      * @return
      */
-    public MemoryAssetBundle addSubAsset(Asset subAsset) {
-        if (subAssetMap.size() < 1) {
-            return create(subAsset);
-        }
-        List<Asset> allAsset = new ArrayList(subAssetMap.values());
-        allAsset.add(subAsset);
-        return create(allAsset);
-    }
-
-    /**
-     * API to add list of sub-asset in give bundleAsset
-     *
-     * @param subAssetList
-     * @return
-     */
-    public MemoryAssetBundle addAllSubAsset(List<Asset> subAssetList) {
-        List<Asset> allAsset = new ArrayList(subAssetMap.values());
-        allAsset.addAll(subAssetList);
-        return create(allAsset);
+    public MemoryAssetBundle addSubAsset(String name, Asset subAsset) {
+        Map<String, Object> metaData = getMetadata();
+        Map<String, String> content = (Map<String, String>) metaData.get("contents");
+        String bundleName = metaData.get("name").toString();
+        content.put(name, "AssetId: "+subAsset.getAssetID());
+        return create(bundleName, content);
     }
 
 
--- src/test/java/sg/dex/starfish/impl/memory/TestMemoryAssetBundle.java
+++ src/test/java/sg/dex/starfish/impl/memory/TestMemoryAssetBundle.java
@@ -3,8 +3,8 @@ package sg.dex.starfish.impl.memory;
 import org.junit.Test;
 import sg.dex.starfish.Asset;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -15,102 +15,52 @@ public class TestMemoryAssetBundle {
     public void testAddAssetInNewBundle() {
         byte data[] = {2, 3, 4};
         Asset a = MemoryAsset.create(data);
-        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create(a);
+        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create("test","abc",a);
 
-        Asset newAsset = memoryAssetBundle.getSubAssetById(a.getAssetID());
-        assertEquals(newAsset.getAssetID(), a.getAssetID());
-
-    }
-
-    @Test
-    public void testAddAssetInExistingBundle() {
-        byte data1[] = {2, 3, 4};
-        Asset a1 = MemoryAsset.create(data1);
-        MemoryAssetBundle memoryAssetBundle1 = MemoryAssetBundle.create(a1);
-        Asset newAsset = memoryAssetBundle1.getSubAssetById(a1.getAssetID());
-        assertEquals(newAsset.getAssetID(), a1.getAssetID());
+        System.out.println(memoryAssetBundle.getAllSubAsset());
+        System.out.println(memoryAssetBundle.getMetadata());
+        System.out.println(memoryAssetBundle.getAssetID());
+        System.out.println(memoryAssetBundle.getSubAssetById("abc"));
 
 
-        byte data2[] = {2, 3, 4, 6};
-        Asset a2 = MemoryAsset.create(data2);
-
-        MemoryAssetBundle memoryAssetBundle2 = memoryAssetBundle1.addSubAsset(a2);
-
-        Asset a1_new = memoryAssetBundle2.getSubAssetById(a1.getAssetID());
-        Asset a2_new = memoryAssetBundle2.getSubAssetById(a2.getAssetID());
-
-        assertEquals(a1_new.getAssetID(), a1.getAssetID());
-        assertEquals(a2_new.getAssetID(), a2.getAssetID());
 
 
     }
 
     @Test
-    public void testisBundleAsset() {
-        byte data[] = {2, 3, 4};
-        Asset a = MemoryAsset.create(data);
-        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create(a);
-        assertEquals(true, memoryAssetBundle.isBundle());
-
-    }
-
-    @Test(expected = Exception.class)
-    public void testisBundleAssetContent() {
-        byte data[] = {2, 3, 4};
-        Asset a = MemoryAsset.create(data);
-        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create(a);
-        memoryAssetBundle.getContent();
-
-    }
-
-    @Test
-    public void testisBundleAssetID() {
-        byte data[] = {2, 3, 4};
-        Asset a = MemoryAsset.create(data);
-        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create(a);
-        assertNotNull(memoryAssetBundle.getAssetID());
-
-    }
-
-    @Test
-    public void testForAssetList() {
-
+    public void testAddAssetInNewBundle2() {
         byte data[] = {2, 3, 4};
         Asset a = MemoryAsset.create(data);
 
-        byte data1[] = {2, 3, 4, 6};
-        Asset a1 = MemoryAsset.create(data1);
+        Map<String,String> map1 = new HashMap<>();
+        map1.put("abc","1234");
+        map1.put("def","567");
+        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create("test",map1);
 
-        List<Asset> assetList = new ArrayList<>();
-        assetList.add(a);
-        assetList.add(a1);
-        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create(assetList);
-        assertEquals(memoryAssetBundle.getAllSubAsset().size(), 2);
-        assertEquals(memoryAssetBundle.getSubAssetById(a.getAssetID()).getAssetID(), a.getAssetID());
-        assertEquals(memoryAssetBundle.getSubAssetById(a1.getAssetID()).getAssetID(), a1.getAssetID());
+        assertEquals(memoryAssetBundle.getAllSubAsset().size(),2);
 
+        System.out.println(memoryAssetBundle.getMetadata().get("contents"));
+        System.out.println(memoryAssetBundle.getMetadata());
+        assertNotNull(memoryAssetBundle.getMetadata().get("contents"));
 
     }
     @Test
-    public void addAllSubAsset(){
-        byte data1[] = {2, 3, 4};
-        Asset a1 = MemoryAsset.create(data1);
-        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create(a1);
+    public void testAddSubAsset() {
+        byte data[] = {2, 3, 4};
+        Asset a = MemoryAsset.create(data);
 
-        byte data2[] = {2, 3, 4,5,6,7};
-        Asset a2 = MemoryAsset.create(data2);
 
-        byte data3[] = {2, 3, 4, 6,2,3,4,};
-        Asset a3 = MemoryAsset.create(data3);
+        MemoryAssetBundle memoryAssetBundle = MemoryAssetBundle.create("TestBundle","first Asset",a.getAssetID());
 
-        List<Asset> assetList = new ArrayList<>();
-        assetList.add(a2);
-        assetList.add(a3);
-        MemoryAssetBundle updatedMBA =memoryAssetBundle.addAllSubAsset(assetList);
-        assertEquals(updatedMBA.getSubAssetById(a1.getAssetID()).getAssetID(),a1.getAssetID());
-        assertEquals(updatedMBA.getSubAssetById(a2.getAssetID()).getAssetID(),a2.getAssetID());
-        assertEquals(updatedMBA.getSubAssetById(a3.getAssetID()).getAssetID(),a3.getAssetID());
+        byte data2[] = {12, 13, 14};
+        Asset a2 = MemoryAsset.create(data);
+        memoryAssetBundle.addSubAsset("Second Asset",a2.getAssetID());
 
+        assertEquals(memoryAssetBundle.getAllSubAsset().size(),2);
+        System.out.println(memoryAssetBundle.getMetadata());
+        assertNotNull(memoryAssetBundle.getMetadata().get("contents"));
 
     }
+
+
 }
