7bc58a692cd5ee814a473da6815785376e9b619a
Mike <mike@mikera.net>
2019-04-16 10:54:11 +0800

    Add property based building of OceanAPI

--- src/test/java/sg/dex/starfish/samples/squid/CommonsAssetTest.java
+++ src/test/java/sg/dex/starfish/samples/squid/CommonsAssetTest.java
@@ -1,5 +1,12 @@
 package sg.dex.starfish.samples.squid;
 
+import java.util.Properties;
+
+import com.oceanprotocol.squid.api.OceanAPI;
+import com.oceanprotocol.squid.api.config.OceanConfig;
+import com.oceanprotocol.squid.exceptions.InitializationException;
+import com.oceanprotocol.squid.exceptions.InvalidConfiguration;
+
 /**
  * Test class for working with a Ocean Commons Marketplace asset
  * @author Mike
@@ -7,8 +14,29 @@ package sg.dex.starfish.samples.squid;
  */
 public class CommonsAssetTest {
 	
-	public static void main(String... args) {
-		
+	public static OceanAPI buildOceanAPI() throws InitializationException, InvalidConfiguration {
+		// Default values for KEEPER_URL, KEEPER_GAS_LIMIT, KEEPER_GAS_PRICE, AQUARIUS_URL, SECRETSTORE_URL, CONSUME_BASE_PATH
+		Properties properties = new Properties();
+		properties.put(OceanConfig.MAIN_ACCOUNT_ADDRESS, "0x00Bd138aBD70e2F00903268F3Db08f2D25677C9e");
+		properties.put(OceanConfig.MAIN_ACCOUNT_PASSWORD,"node0");
+		properties.put(OceanConfig.MAIN_ACCOUNT_CREDENTIALS_FILE,"src/test/resources/accounts/parity/00bd138abd70e2f00903268f3db08f2d25677c9e.json.testaccount");
+		properties.put(OceanConfig.DID_REGISTRY_ADDRESS,"0x4A0f7F763B1A7937aED21D63b2A78adc89c5Db23");
+		properties.put(OceanConfig.AGREEMENT_STORE_MANAGER_ADDRESS, "0x62f84700b1A0ea6Bfb505aDC3c0286B7944D247C");
+		properties.put(OceanConfig.LOCKREWARD_CONDITIONS_ADDRESS, "0xE30FC30c678437e0e8F78C52dE9db8E2752781a0");
+		properties.put(OceanConfig.ESCROWREWARD_CONDITIONS_ADDRESS, "0xeD4Ef53376C6f103d2d7029D7E702e082767C6ff");
+		properties.put(OceanConfig.ESCROW_ACCESS_SS_CONDITIONS_ADDRESS, "0xfA16d26e9F4fffC6e40963B281a0bB08C31ed40C");
+		properties.put(OceanConfig.ACCESS_SS_CONDITIONS_ADDRESS, "0x45DE141F8Efc355F1451a102FB6225F1EDd2921d");
+		properties.put(OceanConfig.TEMPLATE_STORE_MANAGER_ADDRESS, "0x9768c8ae44f1dc81cAA98F48792aA5730cAd2F73");
+		properties.put(OceanConfig.TOKEN_ADDRESS, "0x9861Da395d7da984D5E8C712c2EDE44b41F777Ad");
+		properties.put(OceanConfig.DISPENSER_ADDRESS, "0x865396b7ddc58C693db7FCAD1168E3BD95Fe3368");
+		properties.put(OceanConfig.PROVIDER_ADDRESS, "0x413c9ba0a05b8a600899b41b0c62dd661e689354");
+
+		OceanAPI oceanAPIFromProperties = OceanAPI.getInstance(properties);
+		return oceanAPIFromProperties;
+	}
+	
+	public static void main(String... args) throws InitializationException, InvalidConfiguration {
+		OceanAPI oceanAPI=buildOceanAPI();
 	}
 
 }
