f274701ad041918a4371be1151a41ddc714c1184
Mike <mike@mikera.net>
2019-04-16 10:48:09 +0800

    Add sample class for Commons Asset

---
+++ src/test/java/sg/dex/starfish/samples/squid/CommonsAssetTest.java
@@ -0,0 +1,14 @@
+package sg.dex.starfish.samples.squid;
+
+/**
+ * Test class for working with a Ocean Commons Marketplace asset
+ * @author Mike
+ *
+ */
+public class CommonsAssetTest {
+	
+	public static void main(String... args) {
+		
+	}
+
+}
