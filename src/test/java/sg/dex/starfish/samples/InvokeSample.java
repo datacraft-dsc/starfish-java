4bd605fa43e15177dbaafc95307f2610f762aa41
AyushVerma2 <ayushverma2@gmail.com>
2019-04-30 17:51:03 +0800

    Updated the review comments and modified code for Sync call

--- src/test/java/sg/dex/starfish/samples/InvokeSample.java
+++ src/test/java/sg/dex/starfish/samples/InvokeSample.java
@@ -1,17 +1,12 @@
 package sg.dex.starfish.samples;
 
-import sg.dex.starfish.Operation;
-import sg.dex.starfish.impl.operations.ReverseBytesOperation;
-
-import static junit.framework.TestCase.assertNotNull;
-
 public class InvokeSample {
 
 
 	public static void main(String... args) {
 
-		Operation op = ReverseBytesOperation.create("test");
-		assertNotNull(op.getMetadata());
+//		Operation op = ReverseBytesOperation.create("test");
+//		assertNotNull(op.getMetadata());
 		//System.out.println(JSON.toPrettyString(op.getMetadata()));
 	}
 
