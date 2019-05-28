e697e544cdfa1db99b2a061b19d046ed26f90169
shark8me <kiran.karkera@gmail.com>
2019-05-28 08:59:34 +0800

    add support for prov metadata created with Invoke

--- src/main/java/sg/dex/starfish/util/ProvUtil.java
+++ src/main/java/sg/dex/starfish/util/ProvUtil.java
@@ -1,12 +1,15 @@
 package sg.dex.starfish.util;
 
 import java.util.List;
+import java.util.Collection;
+import sg.dex.starfish.Asset;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.UUID;
 import java.io.OutputStream;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -30,11 +33,7 @@ public class ProvUtil{
         return pre;
     }
 
-
-    /*
-     * Returns a Entity referring to "this", the asset being registered
-     */
-    public static Map<String,Object> createThisInput(){
+    private static Map<String,Object> createAssetEntity(String id){
         Map<String,Object> type=new HashMap<String,Object>();
         type.put("$",OPF+":asset");
         type.put("type","xsd:string");
@@ -42,10 +41,23 @@ public class ProvUtil{
         Map<String,Object> typeContainer=new HashMap<String,Object>();
         typeContainer.put("prov:type",type);
         Map<String,Object> thisContainer=new HashMap<String,Object>();
-        thisContainer.put(OPF+":this",typeContainer);
+        thisContainer.put(OPF+":"+id,typeContainer);
         return thisContainer;
     }
 
+    /*
+     * Returns a Entity referring to "this", the asset being registered
+     */
+    public static Map<String,Object> createThisInput(){
+        return createAssetEntity("this");
+    }
+
+    /*
+     * Returns a Entity referring to asset in the argument
+     */
+    public static Map<String,Object> createAssetDependency(Asset ast){
+        return createAssetEntity(ast.getAssetID());
+    }
 
     private static Map<String,Object> jsonMapEntry(String key,Map<String,Object> ent[]){
         Map<String,Object> eContainer=new HashMap<String,Object>();
@@ -122,6 +134,24 @@ public class ProvUtil{
         return ret;
     }
 
+    /*
+     * Returns a Map representing an derived by Relationship, which
+     * connects the dependent entities
+     */
+    public static Map<String,Object> derivedFrom(Collection<Asset> entities ){
+        Map<String,Object> asid=new HashMap<String,Object>();
+        for(Asset ast: entities){
+            Map<String,Object> type=new HashMap<String,Object>();
+            type.put("prov:usedEntity",ast.getAssetID());
+            type.put("prov:generatedEntity",OPF+"this");
+            asid.put("_:"+UUID.randomUUID().toString(),type);
+        }
+        Map<String,Object> ret=new HashMap<String,Object>();
+        ret.put("wasDerivedFrom",asid);
+        return ret;
+    }
+
+
     public static enum ActivityType { PUBLISH, IMPORT, OPERATION};
 
     /*
@@ -140,6 +170,23 @@ public class ProvUtil{
     }
 
     /*
+     * Returns an Activity identified by activity id and type
+     */
+    public static Map<String,Object> createActivity(String actId,ActivityType activityType,
+                                                    Map<String,Object> actEntries){
+        Map<String,Object> type=new HashMap<String,Object>();
+        type.put("$",OPF+":"+activityType.toString());
+        type.put("type","xsd:string");
+
+        Map<String,Object> typeContainer=new HashMap<String,Object>();
+        typeContainer.put("prov:type",type);
+        typeContainer.putAll(actEntries);
+        Map<String,Object> thisContainer=new HashMap<String,Object>();
+        thisContainer.put(OPF+":"+actId,typeContainer);
+        return thisContainer;
+    }
+
+    /*
      * Returns a list of Activities 
      */
     public static Map<String,Object> createActivities(Map<String,Object>... acts){
@@ -150,24 +197,79 @@ public class ProvUtil{
      * Create a default Prov object with the prefixes and the "this" entity
      */
     public static Map<String,Object> getBaseProvObject(){
-        Map<String,Object> e = createEntities(createThisInput());
         Map<String,Object> res=defaultPrefix();
-        res.putAll(e);
         return res;
     }
 
     /**
+     * Create default provenance metadata for publishing an asset. This create a random
+     * activity ID for tracking the provenance
+     */
+    public static Map<String,Object> createPublishProvenance(String agentId) {
+        String actId=UUID.randomUUID().toString();
+        return createPublishProvenance(actId, agentId);
+    }
+    /**
      * Create default provenance metadata for publishing an asset
      */
     public static Map<String,Object> createPublishProvenance(String actId,
-                                        String agentId) {
+                                                             String agentId) {
         Map<String,Object> a=getBaseProvObject();
+        Map<String,Object> e =createEntities(createThisInput());
         Map<String,Object> act=createActivity(actId,ActivityType.PUBLISH);
         Map<String,Object> agent=createAgent(agentId,AgentIdType.ACCOUNT);
 
         a.putAll(createActivities(act));
+        a.putAll(e);
         a.putAll(createAgents(agent));
         a.putAll(associatedWith(agentId,actId));
+        a.putAll(generatedBy("this",actId));
+        return a;
+    }
+
+    /**
+     * Create dependencies. There are 2 sets of dependencies: one that
+     * are assets, and the second which is JSON payloads.
+     */
+    public static Map<String,Object> createDependencies(String params,
+                                                        String resultParamName){
+        Map<String,Object> ret=new HashMap<String,Object>();
+        Map<String,Object> p=new HashMap<String,Object>();
+        p.put("$",resultParamName);
+        p.put("type","xsd:string");
+        ret.put(OPF+":resultParamName",p);
+
+        p=new HashMap<String,Object>();
+        p.put("$",params);
+        p.put("type","xsd:string");
+        ret.put(OPF+":params",p);
+
+        return ret;
+    }
+
+    /**
+     * Create default provenance metadata for publishing an asset created using invoke.
+     */
+    public static Map<String,Object> createInvokeProvenance(String actId, String agentId,
+                                                            Collection<Asset> assetDependencies,
+                                                            String params,
+                                                            String resultParamName) {
+        Map<String,Object> a=getBaseProvObject();
+        Map<String,Object> e= new HashMap<String,Object>();
+        e.putAll(createThisInput());
+        for(Asset k:assetDependencies){
+            e.putAll(createAssetDependency(k));
+        }
+        Map<String,Object> deps=createDependencies(params, resultParamName);
+
+        Map<String,Object> act=createActivity(actId,ActivityType.OPERATION,deps);
+        Map<String,Object> agent=createAgent(agentId,AgentIdType.ACCOUNT);
+
+        a.putAll(createEntities(e));
+        a.putAll(createActivities(act));
+        a.putAll(createAgents(agent));
+        a.putAll(associatedWith(agentId,actId));
+        a.putAll(derivedFrom(assetDependencies));
         a.putAll(generatedBy("this",actId));
         return a;
     }
--- src/test/java/sg/dex/starfish/integration/developerTC/ProvUtil_23.java
+++ src/test/java/sg/dex/starfish/integration/developerTC/ProvUtil_23.java
@@ -1,10 +1,12 @@
-package sg.dex.starfish.util;
+package sg.dex.starfish.integration.developerTC;
 
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Before;
+import sg.dex.starfish.Asset;
 import org.junit.Test;
 import java.io.FileOutputStream;
 import java.io.File;
@@ -20,11 +22,29 @@ import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+import sg.dex.starfish.Asset;
+import sg.dex.starfish.impl.memory.MemoryAsset;
+import sg.dex.starfish.impl.remote.RemoteAgent;
+import sg.dex.starfish.impl.remote.RemoteAsset;
+import sg.dex.starfish.util.ProvUtil;
+import sg.dex.starfish.util.JSON;
 
 import java.util.Map;
 import java.util.HashMap;
 
-public class ProvUtilTest {
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ProvUtil_23{
+
+    private RemoteAgent remoteAgent;
+
+    @Before
+    public void setup() {
+        remoteAgent = RemoteAgentConfig.getRemoteAgent();
+    }
+
 
     @Test
     public void testProvA() {
@@ -83,4 +103,49 @@ public class ProvUtilTest {
         }
     }
 
+    public Asset uploadAsset() {
+        byte[] data = new byte[]{1, 2, 3};
+        Asset asset1 = MemoryAsset.create(data);
+
+        RemoteAsset remoteAsset = (RemoteAsset)remoteAgent.registerAsset(asset1);
+
+        remoteAgent.uploadAsset(asset1);
+        return asset1;
+    }
+
+    @Test
+    public void testInvokeProv() {
+        try{
+            File temp = new File(System.getProperty("java.io.tmpdir")+
+                                 System.getProperty("file.separator")+
+                                 "invokeprov.json");
+            Writer w= new FileWriter(temp);
+            String actId=UUID.randomUUID().toString();
+            String agentId=UUID.randomUUID().toString();
+            Asset ast = uploadAsset();
+            List<Asset> lst=Arrays.asList(ast);
+            Map<String,Object> p =new HashMap<String,Object>();
+            p.put("did",ast.getAssetID());
+
+            Map<String,Object> p1 =new HashMap<String,Object>();
+            p1.put("to-hash",p);
+
+            String params = JSON.toString(p1);
+            String jsonstring=
+                JSON.toPrettyString(ProvUtil.createInvokeProvenance(actId,agentId, lst,
+                                                                                  params,
+                                                                                  "hash-val"));
+            try{
+                JSON.validateJson(jsonstring);
+            }catch (Exception e){
+                fail("invalid json ");
+            }
+            w.write(jsonstring);
+            w.flush();
+            assertTrue(temp.length()>0);
+
+        }catch(IOException ioe){
+            fail("file has length = 0");
+        }
+    }
 }
