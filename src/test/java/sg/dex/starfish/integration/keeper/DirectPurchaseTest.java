89bd3bf70072d30efd3b2a54be508a1eda8853e7
Ilia Bukalov <ilyabukalov@gmail.com>
2019-09-20 13:46:54 +0800

    Direct purchase integration test done

---
+++ src/test/java/sg/dex/starfish/integration/keeper/DirectPurchaseTest.java
@@ -0,0 +1,60 @@
+package sg.dex.starfish.integration.keeper;
+
+import org.junit.Test;
+
+import com.oceanprotocol.squid.exceptions.EthereumException;
+import org.web3j.crypto.CipherException;
+import java.io.IOException;
+
+import java.math.BigInteger;
+import com.oceanprotocol.squid.models.Account;
+import com.oceanprotocol.squid.models.Balance;
+import sg.dex.starfish.integration.squid.SquidHelperTest;
+import sg.dex.starfish.keeper.DirectPurchaseAdapter;
+import sg.dex.starfish.Ocean;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DirectPurchaseTest {
+    private DirectPurchaseAdapter directPurchaseAdapter;
+    private byte[] reference;
+    private BigInteger tokenAmount;
+
+    public DirectPurchaseTest() throws IOException, CipherException {
+        directPurchaseAdapter = new DirectPurchaseAdapter();
+        reference = new byte[32];
+        reference[0] = 10;
+        tokenAmount = BigInteger.valueOf(10);
+    }
+
+    @Test
+    public void Purchase() throws EthereumException {
+
+        String receiverAddress = "0x068Ed00cF0441e4829D9784fCBe7b9e26D4BD8d0";
+        String receiverPasswd = "secret";
+
+        String senderAddress = "0x00bd138abd70e2f00903268f3db08f2d25677c9e";
+        String senderPasswd = "node0";
+
+        Account senderAccount = new Account(senderAddress, senderPasswd);
+        Account receiverAccount = new Account(receiverAddress, receiverPasswd);
+
+        Ocean ocean = SquidHelperTest.getOcean();
+
+        BigInteger amount = ocean.requestTokens(tokenAmount);
+        assertEquals(amount, tokenAmount);
+
+        Balance balanceSenderBefore = ocean.getBalance(senderAccount);
+        Balance balanceReceiverBefore = ocean.getBalance(receiverAccount);
+
+        boolean transferred = directPurchaseAdapter.sendTokenAndLog(receiverAddress, tokenAmount, reference, reference);
+
+        assertTrue(transferred);
+        Balance balanceSenderAfter = ocean.getBalance(senderAccount);
+        Balance balanceReceiverAfter = ocean.getBalance(receiverAccount);
+        assertEquals(balanceSenderBefore.getDrops().subtract(balanceSenderAfter.getDrops()), tokenAmount);
+        assertEquals(balanceReceiverAfter.getDrops().subtract(balanceReceiverBefore.getDrops()), tokenAmount);
+    }
+}
+
