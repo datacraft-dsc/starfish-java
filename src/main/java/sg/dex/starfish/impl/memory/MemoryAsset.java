f34d3d4003576b2dabf5515237cd84b4e8e785c6
AyushVerma2 <ayushverma2@gmail.com>
2019-11-19 11:37:20 +0800

    removed unused imports

--- src/main/java/sg/dex/starfish/impl/memory/MemoryAsset.java
+++ src/main/java/sg/dex/starfish/impl/memory/MemoryAsset.java
@@ -22,7 +22,13 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
-import static sg.dex.starfish.constant.Constant.*;
+import static sg.dex.starfish.constant.Constant.DATA_SET;
+import static sg.dex.starfish.constant.Constant.DATE_CREATED;
+import static sg.dex.starfish.constant.Constant.TYPE;
+import static sg.dex.starfish.constant.Constant.SIZE;
+import static sg.dex.starfish.constant.Constant.OCTET_STREAM;
+import static sg.dex.starfish.constant.Constant.ID;
+import static sg.dex.starfish.constant.Constant.CONTENT_TYPE;
 
 /**
  * Class representing a local in-memory data asset.
