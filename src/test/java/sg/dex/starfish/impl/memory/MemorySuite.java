e2f7929f84568933b00bf84f2f8658d1a5a7f879
Mike <mike@mikera.net>
2020-02-11 10:48:39 +0800

    Update test library deps, dependency cleanup

--- src/test/java/sg/dex/starfish/impl/memory/MemorySuite.java
+++ src/test/java/sg/dex/starfish/impl/memory/MemorySuite.java
@@ -1,11 +1,6 @@
 package sg.dex.starfish.impl.memory;
 
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
-@SelectPackages("sg.dex.starfish.impl.memory")
 public class MemorySuite {
 
 }
