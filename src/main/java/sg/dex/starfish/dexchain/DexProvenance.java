44687131883e1a8afb9760e2d603d40c66322633
Ilia Bukalov <ilyabukalov@gmail.com>
2020-03-02 16:21:20 +0800

    Search by user and assetId implemented

--- src/main/java/sg/dex/starfish/dexchain/DexProvenance.java
+++ src/main/java/sg/dex/starfish/dexchain/DexProvenance.java
@@ -9,6 +9,7 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.utils.Numeric;
 import sg.dex.starfish.dexchain.impl.Provenance;
 import sg.dex.starfish.exception.DexChainException;
+import sg.dex.starfish.util.Hex;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -95,6 +96,40 @@ public class DexProvenance {
         filter.addSingleTopic(EventEncoder.encode(Provenance.ASSETREGISTERED_EVENT));
         String didTopic = "0x" + assetId;
         filter.addOptionalTopics(didTopic);
+        Flowable<Provenance.AssetRegisteredEventResponse> floable = contract.assetRegisteredEventFlowable(filter);
+        ArrayList<DexProvenanceResult> outcome = new ArrayList<>();
+        floable.subscribe(log -> {
+            DexProvenanceResult newResult = new DexProvenanceResult();
+            newResult.timeStamp = new java.util.Date(log._timestamp.longValue() * 1000);
+            newResult.user = log._user;
+            outcome.add(newResult);
+        });
+        return  outcome;
+    }
+
+    /**
+     * Search asset provenance from chain by user and assetId
+     *
+     * @param String user
+     * @param String assetId
+     * @return Array of DexProvenanceResult. Results of all records related to given asset
+     * @throws DexChainException
+     */
+    public ArrayList<DexProvenanceResult> getAssetProvenance(String assetId, String user) throws DexChainException {
+        BigInteger blockNumber;
+        try {
+            blockNumber = contract.getBlockNumber(Numeric.hexStringToByteArray(assetId)).send();
+        } catch (Exception e) {
+            throw new DexChainException(e);
+        }
+
+        String user_padded = Hex.toZeroPaddedHex(user);
+        String assetId_padded = Hex.toZeroPaddedHex(assetId);
+
+        EthFilter filter = new EthFilter(DefaultBlockParameter.valueOf(blockNumber), DefaultBlockParameterName.LATEST, contract.getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(Provenance.ASSETREGISTERED_EVENT));
+        filter.addSingleTopic(assetId_padded);
+        filter.addSingleTopic(user_padded);
         Flowable<Provenance.AssetRegisteredEventResponse> floable = contract.assetRegisteredEventFlowable(filter);
         ArrayList<DexProvenanceResult> outcome = new ArrayList<>();
         floable.subscribe(log -> {
