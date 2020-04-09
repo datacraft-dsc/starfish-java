63b6c5be87df197f3c5e1957dd54f6ddf12d9b84
AyushVerma2 <ayushverma2@gmail.com>
2020-04-09 11:37:20 +0530

    REfactroing  Starfish-java

--- src/integration-test/java/keeper/DirectPurchaseTestIT.java
+++ src/integration-test/java/keeper/DirectPurchaseTestIT.java
@@ -1,67 +1,63 @@
-package keeper;
-
-import com.oceanprotocol.squid.api.OceanAPI;
-import com.oceanprotocol.squid.exceptions.EthereumException;
-import com.oceanprotocol.squid.models.Account;
-import org.junit.jupiter.api.Test;
-import org.web3j.crypto.CipherException;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import sg.dex.starfish.dexchain.DexConfig;
-import sg.dex.starfish.dexchain.DexConfigFactory;
-import sg.dex.starfish.dexchain.DirectPurchaseAdapter;
-import sg.dex.starfish.impl.squid.SquidService;
-
-import java.io.IOException;
-import java.math.BigInteger;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class DirectPurchaseTestIT {
-    private static final String reference = "0x000000000000a46daef00000000000"; // must be any hardcoded value
-    private static final BigInteger tokenAmount = BigInteger.valueOf(10);
-    private DirectPurchaseAdapter directPurchaseAdapter;
-    private Account senderAccount;
-    private Account receiverAccount;
-
-    public DirectPurchaseTestIT() throws IOException, CipherException {
-        DexConfig dexConfig = DexConfigFactory.getDexConfig("application_test.properties");
-
-        directPurchaseAdapter = DirectPurchaseAdapter.create(dexConfig);
-
-        String senderAddress = dexConfig.getMainAccountAddress();
-        String receiverAddress = dexConfig.getParityAccountAddress();
-        String senderPasswd = dexConfig.getMainAccountPassword();
-        String receiverPasswd = dexConfig.getParityAccountPassword();
-
-        senderAccount = new Account(senderAddress, senderPasswd);
-        receiverAccount = new Account(receiverAddress, receiverPasswd);
-    }
-
-    @Test
-    public void testPurchase() throws EthereumException {
-
-        SquidService squidService = SquidService.create("application_test.properties");
-        OceanAPI oceanAPI = squidService.getOceanAPI();
-        TransactionReceipt transactionReceipt = oceanAPI.getAccountsAPI().requestTokens(tokenAmount);
-        assertTrue(transactionReceipt.isStatusOK());
-
-        BigInteger balanceSenderBefore = oceanAPI.getAccountsAPI().balance(senderAccount).getDrops();
-        BigInteger balanceReceiverBefore = oceanAPI.getAccountsAPI().balance(receiverAccount).getDrops();
-
-        TransactionReceipt receipt = directPurchaseAdapter.sendTokenAndLog(receiverAccount.getAddress(), tokenAmount, reference, reference);
-        assertTrue(receipt.isStatusOK());
-
-        BigInteger balanceSenderAfter = oceanAPI.getAccountsAPI().balance(senderAccount).getDrops();
-        BigInteger balanceReceiverAfter = oceanAPI.getAccountsAPI().balance(receiverAccount).getDrops();
-        assertEquals(tokenAmount, balanceSenderBefore.subtract(balanceSenderAfter));
-        assertEquals(tokenAmount, balanceReceiverAfter.subtract(balanceReceiverBefore));
-    }
-
-    @Test
-    public void testCheckIsPaid() {
-        boolean paid = directPurchaseAdapter.checkIsPaid(senderAccount.getAddress(), receiverAccount.getAddress(), tokenAmount, reference);
-        assertTrue(paid);
-    }
-}
-
+//package keeper;
+//
+//import org.junit.jupiter.api.Test;
+//import org.web3j.crypto.CipherException;
+//import org.web3j.protocol.core.methods.response.TransactionReceipt;
+//import sg.dex.starfish.dexchain.DexConfig;
+//import sg.dex.starfish.dexchain.DexConfigFactory;
+//import sg.dex.starfish.impl.squid.SquidService;
+//
+//import java.io.IOException;
+//import java.math.BigInteger;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//public class DirectPurchaseTestIT {
+//    private static final String reference = "0x000000000000a46daef00000000000"; // must be any hardcoded value
+//    private static final BigInteger tokenAmount = BigInteger.valueOf(10);
+//    private DirectPurchaseAdapter directPurchaseAdapter;
+//    private Account senderAccount;
+//    private Account receiverAccount;
+//
+//    public DirectPurchaseTestIT() throws IOException, CipherException {
+//        DexConfig dexConfig = DexConfigFactory.getDexConfig("application_test.properties");
+//
+//        directPurchaseAdapter = DirectPurchaseAdapter.create(dexConfig);
+//
+//        String senderAddress = dexConfig.getMainAccountAddress();
+//        String receiverAddress = dexConfig.getParityAccountAddress();
+//        String senderPasswd = dexConfig.getMainAccountPassword();
+//        String receiverPasswd = dexConfig.getParityAccountPassword();
+//
+//        senderAccount = new Account(senderAddress, senderPasswd);
+//        receiverAccount = new Account(receiverAddress, receiverPasswd);
+//    }
+//
+//    @Test
+//    public void testPurchase() throws EthereumException {
+//
+//        SquidService squidService = SquidService.create("application_test.properties");
+//        OceanAPI oceanAPI = squidService.getOceanAPI();
+//        TransactionReceipt transactionReceipt = oceanAPI.getAccountsAPI().requestTokens(tokenAmount);
+//        assertTrue(transactionReceipt.isStatusOK());
+//
+//        BigInteger balanceSenderBefore = oceanAPI.getAccountsAPI().balance(senderAccount).getDrops();
+//        BigInteger balanceReceiverBefore = oceanAPI.getAccountsAPI().balance(receiverAccount).getDrops();
+//
+//        TransactionReceipt receipt = directPurchaseAdapter.sendTokenAndLog(receiverAccount.getAddress(), tokenAmount, reference, reference);
+//        assertTrue(receipt.isStatusOK());
+//
+//        BigInteger balanceSenderAfter = oceanAPI.getAccountsAPI().balance(senderAccount).getDrops();
+//        BigInteger balanceReceiverAfter = oceanAPI.getAccountsAPI().balance(receiverAccount).getDrops();
+//        assertEquals(tokenAmount, balanceSenderBefore.subtract(balanceSenderAfter));
+//        assertEquals(tokenAmount, balanceReceiverAfter.subtract(balanceReceiverBefore));
+//    }
+//
+//    @Test
+//    public void testCheckIsPaid() {
+//        boolean paid = directPurchaseAdapter.checkIsPaid(senderAccount.getAddress(), receiverAccount.getAddress(), tokenAmount, reference);
+//        assertTrue(paid);
+//    }
+//}
+//
--- src/main/java/sg/dex/starfish/impl/squid/SquidAccount.java
+++ src/main/java/sg/dex/starfish/impl/squid/SquidAccount.java
@@ -1,42 +1,42 @@
-package sg.dex.starfish.impl.squid;
-
-import com.oceanprotocol.squid.api.AccountsAPI;
-import com.oceanprotocol.squid.models.Account;
-import sg.dex.starfish.impl.AAccount;
-
-import java.math.BigInteger;
-
-/**
- * Class implementing a Squid Account
- *
- * @author Tom
- */
-public class SquidAccount extends AAccount {
-
-    protected final AccountsAPI accountAPI;
-    protected final Account account;
-
-    public SquidAccount(AccountsAPI accountsAPI, Account account) {
-        super(account.getAddress());
-        this.accountAPI = accountsAPI;
-        this.account = account;
-    }
-
-    public static SquidAccount create(Account account, SquidService squidService) {
-        AccountsAPI accountAPI = squidService.getOceanAPI().getAccountsAPI();
-        return new SquidAccount(accountAPI, account);
-    }
-
-    public String getAddress() {
-        return account.getAddress();
-    }
-
-    public BigInteger getEthBalance() {
-        return account.getBalance().getEth();
-    }
-
-    public BigInteger getOceanBalance() {
-        return account.getBalance().getDrops();
-    }
-
-}
+//package sg.dex.starfish.impl.squid;
+//
+//import com.oceanprotocol.squid.api.AccountsAPI;
+//import com.oceanprotocol.squid.models.Account;
+//import sg.dex.starfish.impl.AAccount;
+//
+//import java.math.BigInteger;
+//
+///**
+// * Class implementing a Squid Account
+// *
+// * @author Tom
+// */
+//public class SquidAccount extends AAccount {
+//
+//    protected final AccountsAPI accountAPI;
+//    protected final Account account;
+//
+//    public SquidAccount(AccountsAPI accountsAPI, Account account) {
+//        super(account.getAddress());
+//        this.accountAPI = accountsAPI;
+//        this.account = account;
+//    }
+//
+//    public static SquidAccount create(Account account, SquidService squidService) {
+//        AccountsAPI accountAPI = squidService.getOceanAPI().getAccountsAPI();
+//        return new SquidAccount(accountAPI, account);
+//    }
+//
+//    public String getAddress() {
+//        return account.getAddress();
+//    }
+//
+//    public BigInteger getEthBalance() {
+//        return account.getBalance().getEth();
+//    }
+//
+//    public BigInteger getOceanBalance() {
+//        return account.getBalance().getDrops();
+//    }
+//
+//}
